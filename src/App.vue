74a08d59f6aa3e1656d4e5010c9efb8e13e084c1
haoqiangqiang <826173335@qq.com>
2020-02-06 18:03:19 +0800

    feat：折线图

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <Rate />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Rate from "./components/Rate.vue";
+
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    Rate,
   }
 };
 </script>
@@ -23,6 +23,7 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  width: 100%;
+  height: 100%;
 }
 </style>
